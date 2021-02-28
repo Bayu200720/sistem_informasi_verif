4dfe6e3dd02db7022fb0b2a731b89f45fee1b033
RicoShurui <48675704+RicoShurui@users.noreply.github.com>
2021-02-28 18:38:47 +0700

    Update download.php

--- download.php
+++ download.php
@@ -5,6 +5,7 @@
 	$sales 	= find_nodin_j_pengajuan_spm($_GET['spm']);
 
 		// Enter the name of directory 
+	$pathPr 	= "uploads/products/";  
 	$pathSPM 	= "uploads/spm/";  
 	$pathSP2D 	= "uploads/sp2d/";  
 	$pathPJ 	= "uploads/pertanggungjawaban/";
@@ -17,6 +18,7 @@
 	$zip = new ZipArchive; 
 
 	if($zip->open($zipcreated, ZipArchive::CREATE ) === TRUE) {  
+		$zip->addFile($pathPr.$sales[0]['upload'], 'Dokumen Pengajuan '." Tgl. ".strtotime(date('d-m-Y')).'.pdf'); 
 		$zip->addFile($pathSPM.$sales[0]['file_spm'], 'Dokumen SPM '." Tgl. ".strtotime(date('d-m-Y')).'.pdf'); 
 		$zip->addFile($pathSP2D.$sales[0]['file_sp2d'], 'Dokumen SP2D '." Tgl. ".strtotime(date('d-m-Y')).'.pdf'); 
 		$zip->addFile($pathSP2D.$sales[0]['upload_pertanggungjawaban'], 'Dokumen Pertanggungjawaban '." Tgl. ".strtotime(date('d-m-Y')).'.pdf'); 
