4c548f267f61cc9d4e7c588b145c018e056f6c53
rankoms <rankom202@gmail.com>
2021-03-21 09:04:21 +0700

    membuat fitur edit bptjb

--- bpp_sptjb.php
+++ bpp_sptjb.php
@@ -9,6 +9,27 @@
 $status= $_GET['status'];
 
 
+if($_GET['status'] == 'h'){
+  $id =$_GET['id'];
+  $query="DELETE FROM detail_pengajuan WHERE id =".$id;
+  $hasil=$db->query($query);
+  
+  if($hasil){
+      $session->msg('s',"Delete Success ");
+      if($user['user_level']==2){
+      redirect('bpp_sptjb.php', false);
+      }else{
+      redirect('bpp_sptjb.php', false);
+      }
+  } else {
+      $session->msg('d',' Sorry failed to added!');
+      if($user['user_level']==2){
+      redirect('bpp_sptjb.php', false);
+  }else{
+      redirect('bpp_sptjb.php', false);
+  }
+  }
+}
  if(isset($_POST['sptjb'])){
    $req_fields = array('no_sptjb','nominal','id_akun','tanggal');
    validate_fields($req_fields);
@@ -53,11 +74,20 @@ $status= $_GET['status'];
      $keterangan   = remove_junk($db->escape($_POST['keterangan']));
      $date    = make_date();
      $id_pengajuan = remove_junk($db->escape($_GET['id']));
-     $query  = "INSERT INTO detail_pengajuan (";
-     $query .=" no_sptjb,nominal,id_akun,keterangan,id_pengajuan,pph,ppn,tanggal_dp";
-     $query .=") VALUES (";
-     $query .=" '{$no_sptjb}', '{$nominal}', '{$akun}', '{$keterangan}', '{$id_pengajuan}','{$pph}','{$ppn}','{$tanggal}'";
-     $query .=")";
+     $id_sptjb = $_POST['id_sptjb'];
+     if($_POST['id_sptjb']){
+
+       $query  = "UPDATE detail_pengajuan SET";
+       $query .=" no_sptjb='{$no_sptjb}',nominal='{$nominal}',id_akun='{$akun}',keterangan='{$keterangan}',pph='{$pph}',ppn='{$ppn}',tanggal_dp='{$tanggal}'";
+       $query .=" WHERE id= '{$id_sptjb}'";
+     }
+     else{
+       $query  = "INSERT INTO detail_pengajuan (";
+       $query .=" no_sptjb,nominal,id_akun,keterangan,id_pengajuan,pph,ppn,tanggal_dp";
+       $query .=") VALUES (";
+       $query .=" '{$no_sptjb}', '{$nominal}', '{$akun}', '{$keterangan}', '{$id_pengajuan}','{$pph}','{$ppn}','{$tanggal}'";
+       $query .=")";
+     }
      if($db->query($query)){
        $session->msg('s',"Detail Pengajuan added ");
        redirect('bpp_sptjb.php?id='.$_GET["id"], false);
@@ -95,7 +125,7 @@ $sales = find_sptb_5pum_tahun($user['id_satker']);
             <span>SPTJB</span>
           </strong>
           <div class="pull-right">
-          <a href="#" class="btn btn-primary" id="editsp2d" data-toggle="modal" data-target="#exampleModal" data-id='<?=$sale['id'];?>'>Add SPTJB</a>
+          <a href="#" class="btn btn-primary" id="editsp2d" data-toggle="modal" data-target="#modal_sptjb" data-id='<?=$sale['id'];?>'>Add SPTJB</a>
           </div>
         </div>
         <div class="panel-body"  style="width: 100%;">
@@ -121,11 +151,10 @@ $sales = find_sptb_5pum_tahun($user['id_satker']);
                <td style="display: none"></td>
             <td class="text-center">
                  <div class="btn-group">
-                 
-                     <a href="#" id="editpencairan" data-toggle="modal" data-target="#EditPanjar" data-id='<?=$sale['id'];?>' data-keterangan='<?=$sale['keterangan'];?>' data-tanggal='<?=$sale['tanggal'];?>' data-nominal='<?=$sale['nominal'];?>' data-spm='<?=$sale['spm'];?>' data-id_satker="<?=$sale['id_satker']?>" data-uraian="<?=$sale['spm']?>" class="btn btn-warning btn-xs" class="btn-edit" title="Edit" >
+                     <a href="#" data-id='<?=$sale['id'];?>' data-tanggal="<?=$sale['tanggal_dp']?>" data-no_sptjb="<?=$sale['no_sptjb']?>" data-id_akun="<?=$sale['id_akun']?>" data-nominal="<?=$sale['nominal']?>" data-pph="<?=$sale['pph']?>" data-ppn="<?=$sale['ppn']?>" data-keterangan="<?=$sale['keterangan']?>" class="btn btn-warning btn-xs btn-edit" title="Edit" >
                        <span class="glyphicon glyphicon-edit"></span>
                      </a>
-                     <a onclick="return confirm('Yakin Hapus?')" href="bpp_sptjb?id=<?php echo (int)$sale['id'];?>&status=pencairan" class="btn btn-danger btn-xs"  title="Delete" data-toggle="tooltip">
+                     <a onclick="return confirm('Yakin Hapus?')" href="bpp_sptjb.php?id=<?=$sale['id'];?>&status=h" class="btn btn-danger btn-xs"  title="Delete" data-toggle="tooltip">
                        <span class="glyphicon glyphicon-trash"></span>
                      </a>
                   </div>
@@ -143,73 +172,19 @@ $sales = find_sptb_5pum_tahun($user['id_satker']);
 
 
 
-<!-- Modal -->
-<div class="modal fade" id="EditPanjar" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Edit Panjar</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <form action="bpp_sptjb" method="POST">
-        <div class="modal-body">
-          <div class="form-group">
-                <label for="exampleInputEmail1">Uraian</label>
-                <select name="uraian" id="uraian" class="form-control">
-                  <?php 
-                    $data = find_all('master_panjar');
-                    foreach($data as $key => $value):?>
-                      <option value="<?php echo $value['name'] ?>"><?php echo $value['name'] ?></option>
-                  <?php endforeach;?>
-                </select>
-            </div>
-            <div class="form-group">
-                <label for="exampleInputEmail1">Tanggal</label>
-                <input type="date" class="form-control" id="tanggal" name="tanggal" placeholder="tanggal">
-                <input type="hidden" class="form-control" id="id" name="id" >
-            </div>
-            <div class="form-group">
-                <label for="exampleInputEmail1">Nominal</label>
-                <input type="text" class="form-control" id="nominal" name="nominal" placeholder="Nominal">
-            </div>
-            <div class="form-group">
-                    <label for="exampleInputEmail1">Satker</label>
-                        <select class="form-control" id="id_satker" name="id_satker">
-                            <option value="">Pilih Satker</option>
-                            <?php $jenis = find_all('satker');?>
-                            <?php  foreach ($jenis as $j): ?>
-                            <option value="<?php echo (int)$j['id'] ?>">
-                                <?php echo $j['keterangan'] ?></option>
-                            <?php endforeach; ?>
-                        </select>
-            </div>
-            <div class="form-group">
-                <label for="exampleInputEmail1">Keterangan</label>
-                <input type="text" class="form-control" id="keterangan" name="keterangan" placeholder="Keterangan">
-            </div>
-        </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="submit" class="btn btn-primary" name="UpdatePanjar">Update</button>
-      </div>
-      </form>
-    </div>
-  </div>
-</div>
 
 <!-- Modal input sp2d-->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog"  aria-hidden="true">
+<div class="modal fade" id="modal_sptjb" tabindex="-1" role="dialog" aria-labelledby="modal_sptjbLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Add SPTJB</h5>
+        <h5 class="modal-title" id="modal_sptjbLabel">Add SPTJB</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <form action="bpp_sptjb.php" method="POST">
+      <input type="hidden" id="id_sptjb" name="id_sptjb">
       <div class="modal-body">
       
         <div class="form-group">
@@ -237,14 +212,14 @@ $sales = find_sptb_5pum_tahun($user['id_satker']);
         <div class="form-group">
           <div class="input-group">
             <div class="input-group-text">
-              <input type="radio" name="pph1" aria-label="Checkbox for following text input" value="pph2p">
+              <input type="radio" class="radio-pajak" name="pph1" aria-label="Checkbox for following text input" value="pph5p">
               <label for="pph2p"><i > PPH 21 5%</i> </label>
-              <input type="radio" name="pph1" aria-label="Checkbox for following text input" value="pph15p">
+              <input type="radio" class="radio-pajak" name="pph1" aria-label="Checkbox for following text input" value="pph15p">
               <label for="pph15p"><i > PPH 21 15%</i> </label>
-              <input type="radio" name="pph1" aria-label="Checkbox for following text input" value="pph5p">       
-              <label for="pph5p"><i > PPH 23 2%</i></label>
-              <input type="radio" name="pph1" aria-label="Checkbox for following text input" value="ppn">       
-              <label for="pph5p"><i > PPN 10%</i></label>
+              <input type="radio" class="radio-pajak" name="pph1" aria-label="Checkbox for following text input" value="pph2p">       
+              <label for="pph2p"><i > PPH 23 2%</i></label>
+              <input type="radio" class="radio-pajak" name="pph1" aria-label="Checkbox for following text input" value="ppn">       
+              <label for="ppn"><i > PPN 10%</i></label>
           </div>
          </div>
         </div>
@@ -272,12 +247,76 @@ $sales = find_sptb_5pum_tahun($user['id_satker']);
 </div>
 
 <?php include_once('layouts/footer.php'); ?>
-
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 <script>
   $(document).ready(function(){
-    $('.btn-edit').on('click', function(){
-      $('#id_satker').val($(this).data('id_satker'));
-      $('#uraian').val($(this).data('uraian'));
+    $('#modal_sptjb').on('hidden.bs.modal', function () { 
+      location.reload(true);
+      // alert('as')
     });
+    $('.radio-pajak').on('click', function(){
+      var nominal = getNum(hapustitik($('#nominal').val()));
+      var pph = 0;
+      var ppn = 0;
+      if($(this).val() == 'pph2p'){
+        pph = nominal * 2/100;
+        $('#pph').val(pph);
+      }
+      if($(this).val() == 'pph15p'){
+        pph = nominal * 15/100;
+        $('#pph').val(pph);
+      }
+      if($(this).val() == 'ppn'){
+        ppn = nominal * 10/100;
+        $('#ppn').val(ppn);
+      }
+      if($(this).val() == 'pph5p'){
+        pph = nominal * 5/100;
+        $('#pph').val(pph);
+      }
+    });
+    $('#nominal').on('keyup', function(){
+
+      var nominal = getNum(hapustitik($('#nominal').val()));
+      var pph = 0;
+      var ppn = 0;
+      if($('.radio-pajak').val() == 'pph2p'){
+        pph = nominal * 2/100;
+        $('#pph').val(pph);
+      }
+      if($('.radio-pajak').val() == 'pph15p'){
+        pph = nominal * 15/100;
+        $('#pph').val(pph);
+      }
+      if($('.radio-pajak').val() == 'ppn'){
+        ppn = nominal * 10/100;
+        $('#ppn').val(ppn);
+      }
+      console.log($('.radio-pajak').val());
+    });
+    $('.btn-edit').on('click', function(e){
+      e.preventDefault();
+      $('#modal_sptjb').modal('show');
+      $('#id_sptjb').val($(this).data('id'));
+      $('#tanggal').val($(this).data('tanggal'));
+      $('#no_sptjb').val($(this).data('no_sptjb'));
+      $('#id_akun').val($(this).data('id_akun'));
+      $('#nominal').val($(this).data('nominal'));
+      $('#pph').val($(this).data('pph'));
+      $('#ppn').val($(this).data('ppn'));
+      $('#keterangan').val($(this).data('keterangan'));
+      $('#modal_sptjbLabel').html('Edit SPTJB');
+    })
+    function hapustitik(angka){
+        var nilai = angka.split('.').join("");
+        return nilai;
+    }
+    function getNum(val) {
+       if (isNaN(val)) {
+         return 0;
+       }
+       return val;
+    }
   })
 </script>
